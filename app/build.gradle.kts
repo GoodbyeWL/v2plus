d4ef7d1172c95d6375665e142d538467087f0cae
7p9eiiwqo8kos <193345660+7p9eiiwqo8kos@users.noreply.github.com>
2026-08-23 15:17:43 +0300

    Release - Bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.v2plus.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 729
-        versionName = "1.2.9"
+        versionCode = 730
+        versionName = "1.3.0"
         multiDexEnabled = true
         resourceConfigurations.addAll(listOf("en", "ru", "be", "uk"))
 
